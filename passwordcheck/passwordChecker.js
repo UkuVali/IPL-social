eb10f048f9106934ae64b4cec29dd5ea8829982f
vaeli-uku <uku.vaeli.001@student.uni.lu>
2025-01-17 17:39:41 +0100

    comment

--- passwordcheck/passwordChecker.js
+++ passwordcheck/passwordChecker.js
@@ -1,19 +1,23 @@
 export function passwordChecker(password) {
-
+    // Vérifi la longueur minimale
     if (password.lenght < 8){
         return false;
     }
+    // Vérifi si il a character special
     const CharSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]/.test(password);
     if (!CharSpecial) {
       return false;
     }
+    // Vérifi si il a chiffre
     const chiffre = /\d/.test(password);
     if (!chiffre) {
       return false;
     }
+    // Vérifi si il a pas ipl
     const hasIpl = password.toLowerCase().includes("ipl");
     if (hasIpl) {
       return false;
     }
+    // Vérification finale
     return true;
 }
