ca59e9783c03aa68b5586917fba20899cea6c65f
vaeli-uku <uku.vaeli.001@student.uni.lu>
2025-01-17 16:36:51 +0100

    charactere special

--- passwordcheck/passwordChecker.js
+++ passwordcheck/passwordChecker.js
@@ -3,6 +3,13 @@ export function passwordChecker(password) {
     if (password.lenght < 8){
         return false;
     }
+    
+    
+    const CharSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]/.test(password);
+    if (!CharSpecial) {
+      return false;
+    }
+
 
     const hasIpl = password.toLowerCase().includes("ipl");
     if (hasIpl) {
